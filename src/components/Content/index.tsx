aea46e43ac8ccded843599a9abbdc953cf55a309
Davit Varamashvili <davit.v@peacock.io>
2023-03-06 16:53:43 +0400

    Added content elements

--- src/components/Content/index.tsx
+++ src/components/Content/index.tsx
@@ -5,22 +5,47 @@ import styles from "./style/style.module.scss";
 
 const interiorElementsList = [
   {
+    id: 0,
     img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
     description: "Original Bath Room (15s ago, took 6s)",
   },
   {
+    id: 1,
     img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
     description: "Original Bath Room (12s ago, took 6s)",
   },
   {
+    id: 2,
     img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
     description: "Original Bath Room (16s ago, took 6s)",
   },
   {
+    id: 3,
     img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
     description: "Original Bath Room (19s ago, took 6s)",
   },
   {
+    id: 4,
+    img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
+    description: "Original Bath Room (11s ago, took 6s)",
+  },
+  {
+    id: 5,
+    img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
+    description: "Original Bath Room (11s ago, took 6s)",
+  },
+  {
+    id: 6,
+    img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
+    description: "Original Bath Room (11s ago, took 6s)",
+  },
+  {
+    id: 7,
+    img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
+    description: "Original Bath Room (11s ago, took 6s)",
+  },
+  {
+    id: 8,
     img: "https://actionaidrecycling.org.uk/wp-content/uploads/2021/01/johann-siemens-EPy0gBJzzZU-unsplash-1208x800.webp",
     description: "Original Bath Room (11s ago, took 6s)",
   },
@@ -36,7 +61,7 @@ const Content = () => {
         columnSpacing={{ xs: 1, sm: 2, md: 0 }}
       >
         {interiorElementsList.map((el) => {
-          return <ContentElement key={el.description} content={el} />;
+          return <ContentElement key={el.id} content={el} />;
         })}
       </Grid>
     </div>
