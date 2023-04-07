52ffde48f966f046aab3de26996b85c7a31a583e
idarbuashvili <iraklid94@gmail.com>
2023-04-07 22:29:47 +0400

    Removed scroll from individual interior modal. Changed site title

--- src/components/Interior/Interior.tsx
+++ src/components/Interior/Interior.tsx
@@ -17,9 +17,24 @@ export default function Interior({ currentInterior }: Props) {
   return (
     <>
       {currentInterior ? (
-        <Grid container spacing={2}>
+        <Grid
+          container
+          spacing={2}
+          style={{
+            height: '85vh',
+          }}
+        >
           {currentInterior.images.map((image, ind) => (
-            <Grid display="flex" className="first-of-type:pl-0 float-left" xs={6} item key={image.img}>
+            <Grid
+              display="flex"
+              className="first-of-type:pl-0 float-left"
+              xs={6}
+              item
+              key={image.img}
+              style={{
+                height: '50%',
+              }}
+            >
               <PredictionCard
                 image={image}
                 prediction={predictionItems.find((pred) => pred.name === image.img)?.predictions}
