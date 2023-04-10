61d1d8cf4bce4119056b75508139dfbec7cd0ced
idarbuashvili <iraklid94@gmail.com>
2023-04-11 00:26:00 +0400

    Adds google analytics

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import Header from '@/components/Header/Header'
 import Interior, { Render } from '@/components/Interior/Interior'
 import NewRender from '@/components/NewRender/NewRender'
 import Head from 'next/head'
+import Script from 'next/script'
 import { useState } from 'react'
 
 // const inter = Inter({ subsets: ['latin'] })
@@ -50,6 +51,16 @@ export default function Home() {
         <Interior currentInterior={interiorCurrent} currentRender={currentRender} setCurrentRender={setCurrentRender} />
       </Dialog>
       {/* <StyleGallery /> */}
+      <Script src="https://www.googletagmanager.com/gtag/js?id=G-B62T0MRV0M" strategy="afterInteractive" />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-B62T0MRV0M');
+        `}
+      </Script>
     </>
   )
 }
