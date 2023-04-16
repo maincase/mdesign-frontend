5975634e7a3d94e194d52204131158b76d425a32
idarbuashvili <iraklid94@gmail.com>
2023-04-16 19:47:26 +0400

    Fix issues in next.config file

--- next.config.js
+++ next.config.js
@@ -1,34 +1,31 @@
-let commitHash
-// let tag
-let branch
-
-try {
-  commitHash = require('child_process').execSync('git rev-parse HEAD').toString().trim()
-} catch {
-  console.log('Error:', 'git rev-parse HEAD:', "Can't get commit hash")
-  process.exit()
-}
-
-// try {
-//   tag = require('child_process').execSync('git describe --tags').toString().trim()
-// } catch {
-//   console.log('Error:', 'git describe --tags:', "Can't get tag")
-//   process.exit()
-// }
-
-try {
-  branch = require('child_process').execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
-} catch {
-  console.log('Error:', 'git rev-parse --abbrev-ref HEAD:', "Can't get branch name")
-  process.exit()
-}
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
 
   generateBuildId: async () => {
+    let commitHash
+    // let tag
+    let branch
+
+    try {
+      commitHash = require('child_process').execSync('git rev-parse HEAD').toString().trim()
+    } catch {
+      console.log('Error:', 'git rev-parse HEAD:', "Can't get commit hash")
+    }
+
+    // try {
+    //   tag = require('child_process').execSync('git describe --tags').toString().trim()
+    // } catch {
+    //   console.log('Error:', 'git describe --tags:', "Can't get tag")
+    // }
+
+    try {
+      branch = require('child_process').execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+    } catch {
+      console.log('Error:', 'git rev-parse --abbrev-ref HEAD:', "Can't get branch name")
+    }
+
     return commitHash
   },
 
