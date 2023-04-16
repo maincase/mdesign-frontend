dc0fab05b511336efa3d7debdba9ae2f491fa863
idarbuashvili <iraklid94@gmail.com>
2023-04-16 14:41:33 +0400

    Add code in next config to include commit hash in build

--- next.config.js
+++ next.config.js
@@ -1,7 +1,36 @@
+let commitHash
+let tag
+let branch
+
+try {
+  commitHash = require('child_process').execSync('git rev-parse HEAD').toString().trim()
+} catch {
+  console.log('Error:', 'git rev-parse HEAD:', "Can't get commit hash")
+  process.exit()
+}
+
+// try {
+//   tag = require('child_process').execSync('git describe --tags').toString().trim()
+// } catch {
+//   console.log('Error:', 'git describe --tags:', "Can't get tag")
+//   process.exit()
+// }
+
+try {
+  branch = require('child_process').execSync('git rev-parse --abbrev-ref HEAD').toString().trim()
+} catch {
+  console.log('Error:', 'git rev-parse --abbrev-ref HEAD:', "Can't get branch name")
+  process.exit()
+}
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   output: 'standalone',
+
+  generateBuildId: async () => {
+    return commitHash
+  },
 
   // images: {
   //   remotePatterns: [
