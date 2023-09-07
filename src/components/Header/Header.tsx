0181ffc785210189f33ee687c73b343fd7a1e693
idarbuashvili <iraklid94@gmail.com>
2023-09-07 19:02:40 +0400

    Removed no more needed files

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,25 +1,18 @@
 'use client'
 
 import { Box, Button } from '@mui/material'
-import clsx from 'clsx'
 import Image from 'next/image'
 import Link from 'next/link'
-import styles from './Header.module.scss'
 
 export default function Header() {
   return (
-    <Box
-      className={clsx(
-        'flex flex-grow p-8 items-center justify-between absolute z-20 top-0 left-0 w-full',
-        styles.header
-      )}
-    >
+    <Box className="flex flex-grow p-8 items-center justify-between absolute z-20 top-0 left-0 w-full">
       <Link className="flex justify-center" href="/">
         <Image src="/logo.svg" width={250} height={45} alt="Logo" className="w-[160px] md:w-[250px]" />
       </Link>
       {/* <h2>INTERIOR DESIGN MOCKUPS AND VIRTUAL STAGING BY AI</h2> */}
       {/* <span>Design Your Space, Empower Your Life</span> */}
-      <div className={styles.header_right}>
+      <div>
         {/* <Button className={clsx(styles.billing_button, styles.button)} variant="text" sx={{ textTransform: 'none' }}>
           Billing
         </Button>
